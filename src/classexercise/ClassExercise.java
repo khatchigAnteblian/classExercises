bbf82588f540fac1c18cd3274b3138cd2fffe782
Khatchig Anteblian <Khatchig.Anteblian@student.tdsb.on.ca>
2018-09-24 13:25:54 -0400

    Modified tester. Fixed formatting. Updated gitignore

--- src/classexercise/ClassExercise.java
+++ src/classexercise/ClassExercise.java
@@ -16,27 +16,43 @@ public class ClassExercise {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//        int[] pNums = {1, 7, 5, -9, 5};
-//        int[] pDens = {3, -5, 6, -12, 8};
-//        int[] qNums = {4, -9, -25, -3, 13};
-//        int[] qDens = {5, -7, -30, 4, 20};
-//        
-//        for (int i=0; i<pNums.length; i++) {
-//            Fraction p = new Fraction(pNums[i], pDens[i]);
-//            Fraction q = new Fraction(qNums[i], qDens[i]);
-//            Fraction r = p.larger(q);
-//
-//            System.out.println(r.num + " / " + r.den);
-//        }
         
-        Fraction a = new Fraction(12, 4);
-        Fraction b = new Fraction(3, 4);
+        // Question 1
+        
+        int[][] p = {{1, 3}, {7, -5}, {5, 6}, {-9, -12}, {5, 8}};
+        int[][] q = {{4, 5}, {-9, -7}, {-25, -30}, {-3, 4}, {13, 20}};
+        
+        for (int i=0; i<p.length; i++) {
+            Fraction pFrac = new Fraction(p[i][0], p[i][1]);
+            Fraction qFrac = new Fraction(q[i][0], q[i][1]);
+            Fraction r = pFrac.larger(qFrac);
+
+            System.out.println(r.num + " / " + r.den);
+        }
+        
+        
+        // Question 2
+        Fraction a = new Fraction(5,6);
+        Fraction b = new Fraction(3,8);
         Fraction c = a.timesEquals(b);
+        System.out.println(a.num + " / " + a.den);
+        System.out.println(b.num + " / " + b.den);
         System.out.println(c.num + " / " + c.den);
-        System.out.println(a.num + " / " + a.den);
-        a = a.reduce();
-        System.out.println(a.num + " / " + a.den);
+        c = c.reduce();
+        System.out.println(c.num + " / " + c.den);
         
+        
+        // Question 3
+        Fraction x = new Fraction(1.5);
+        System.out.println(x.num + " / " + x.den);
+        x = x.reduce();
+        System.out.println(x.num + " / " + x.den);
+        
+        // Addition
+        Fraction y = new Fraction(1,2);
+        Fraction z = new Fraction(2,3);
+        Fraction j = y.plus(z);
+        System.out.println(j.num + " / " + j.den);
     }
     
 }
